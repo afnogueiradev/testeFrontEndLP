f161bdc45453a238bdcfde72fcf16147156f5e59
André Nogueira <afnogueiradev@gmail.com>
2021-01-13 19:54:11 -0300

    js e fim do projeto

--- js/functions.js
+++ js/functions.js
@@ -8,8 +8,8 @@ $('.responsive').slick({
         {
         breakpoint: 1024,
         settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 2,
             infinite: true,
             dots: true
         }
@@ -17,8 +17,8 @@ $('.responsive').slick({
         {
         breakpoint: 600,
         settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 1,
+            slidesToScroll: 1
         }
         },
         {
@@ -30,3 +30,34 @@ $('.responsive').slick({
         }
     ]
 });
+
+$(document).ready(function() {
+    console.log('te')
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('span.btn-topo').fadeIn(200);
+        } else {
+            $('span.btn-topo').fadeOut(200);
+        }
+    });
+    $('span.btn-topo').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 300);
+    })
+
+    $('.btn-enviar').click(function(){
+        var nome = $('#nome').val()
+        var email = $('#email').val()
+        var msg =  $('#mensagem').val()
+        if(nome == ""){$('.error-name').show()}else{$('.error-name').hide()}
+        if(email == ""){$('.error-mail').show()}else{$('.error-mail').hide()}
+        if(msg == ""){$('.error-msg').show()}else{$('.error-msg').hide()}
+        if(nome.length > 0 && email.length > 0 && msg.length > 0){
+            if(msg.length > 10){
+                $('.sucess-msg').show();
+            }else{
+                $('.error-msg').show()
+            }
+        }
+    })
+})
